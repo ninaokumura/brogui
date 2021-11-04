a490e9368aa7bc4dac81c93cb5eac0a1a587e2ba
Nina Okumura <elaineokumura@gmail.com>
2021-11-05 09:30:46 +1300

    display validation errors

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -4,9 +4,13 @@ const path = require('path');
 module.exports = (req, res) => {
   User.create(req.body, (error, user) => {
     if (error) {
+      const validationErrors = Object.keys(error.errors).map(
+        key => error.errors[key].message
+      );
+      req.session.validationErrors = validationErrors;
       return res.redirect('/auth/register');
     }
-    console.log(user);
+
     res.redirect('/');
   });
 };
